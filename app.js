72e27e649e7f6ba08638bc23d0a8c91693719b8f
laibarajpoot <128062421+laibarajpoot@users.noreply.github.com>
2023-05-29 19:00:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+function circle() {
+    var element = document.getElementById('circle');
+    element.style.width = '300px';
+    element.style.height = '300px';
+    element.style.borderRadius = '50%'
+  }
+
+  function rectangle() {
+    var element = document.getElementById('rectangle');
+    element.style.width = '300px';
+    element.style.height = '100px';
+    element.style.borderRadius = '0%'
+  }
+  function square() {
+    var element = document.getElementById('square');
+    element.style.width = '300px';
+    element.style.height = '300px';
+    element.style.borderRadius = '0%'
+  }
+
+  
